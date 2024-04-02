87936e682355ecf005cdf53e8661f78c92ef43d3
João Guilherme Da Rocha <joaoguibc@gmail.com>
2024-04-02 19:29:04 +0000

    Connect dashboard header with APi

---
+++ src/api/get-managed-restaurant.ts
@@ -0,0 +1,18 @@
+import { api } from '@/lib/axios'
+
+interface GetManagedRestaurantResponse {
+  description: string | null
+  id: string
+  name: string
+  createdAt: Date
+  updatedAt: Date
+  managerId: string | null
+}
+
+export async function getManagedRestaurant() {
+  const response = await api.get<GetManagedRestaurantResponse>(
+    '/managed-restaurant',
+  )
+
+  return response.data
+}
